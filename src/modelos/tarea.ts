54353397ceaa27fa52d1d3a0b901437b7d160e80
Martin_Urtiz <urtizbereamartin19@gmail.com>
2025-11-01 17:25:23 -0300

    clase tarea y readline

---
+++ src/modelos/tarea.ts
@@ -0,0 +1,28 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export class Tarea{
+    public id: string;
+    public titulo: string;
+    public descripcion: string;
+    public estado: 'pendiente' | 'en curso' | 'terminada' | 'cancelada';
+    public fechaCreacion: Date;
+    public vencimiento: Date | null;
+    public dificultad: number;
+    public eliminada: boolean;
+
+        constructor(
+        titulo: string,
+        descripcion = '',
+        vencimiento: Date | null = null,
+        dificultad = 1
+    ) {
+        this.id = uuidv4();
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.estado = 'pendiente';
+        this.fechaCreacion = new Date();
+        this.vencimiento = vencimiento;
+        this.dificultad = dificultad;
+        this.eliminada = false;
+    }
+}
