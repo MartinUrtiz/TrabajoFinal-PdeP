aaf6b2a48c97eb24adf6ab381b9fdc8204ba367c
Martin_Urtiz <urtizbereamartin19@gmail.com>
2025-11-29 10:51:42 -0300

    cargar y guardar tareas en el archivo

--- src/data/repositorio.ts
+++ src/data/repositorio.ts
@@ -8,4 +8,21 @@ export class RepositorioTareas {
     constructor(archivo: string = 'tareas.json') {
         this.archivo = archivo;
     }
+
+// Carga tareas del archivo y las convierte en Objetos Tarea
+    async cargarTodas(): Promise<Tarea[]> {
+        try {
+            const datos = await fs.readFile(this.archivo, 'utf-8');
+            const listaCruda = JSON.parse(datos);
+            return listaCruda.map((t: any) => Tarea.fromJSON(t));
+        } catch (error) {
+            return [];
+        }
+    }
+
+    // Guarda el array actual en el archivo
+    async guardarTodas(tareas: Tarea[]): Promise<void> {
+        const datos = JSON.stringify(tareas, null, 2);
+        await fs.writeFile(this.archivo, datos, 'utf-8');
+    }
 }
